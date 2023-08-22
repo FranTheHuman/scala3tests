1a1f999faf27ec72a80c162c13c739d3548661c2
francisco <perrottafrancisco0@gmail.com>
2023-08-22 14:56:23 -0300

    antigivens

--- src/main/scala/AntiGivens.scala
+++ src/main/scala/AntiGivens.scala
@@ -1,3 +1,5 @@
+import AntiGivens.SameType.processListSameTypeV3
+
 object AntiGivens extends App {
 
   def processLists[A, B](la: List[A], lb: List[B]): List[(A, B)] =
@@ -7,7 +9,7 @@ object AntiGivens extends App {
     } yield (a, b)
 
 
-  object SameTypeNaive {
+  object SameType {
 
     // You can modify the code
     def processListsSameType[A](la: List[A], lb: List[A]): List[(A, A)] =
@@ -22,18 +24,33 @@ object AntiGivens extends App {
 
     // You need that compiler determine that A and B are the same type (e.g. the domain needs that A and B are the same type)
     // enforce DIFFERENT types
-    val combinedLists = processLists(List(1, 2, 3), List("a", "b", "c")) // "ok"
-    val notOkLists = processLists(List(1, 2, 3), List(4, 5)) // "not ok"
 
     // type =:=[A, B]
-    def processListSameTypeV3[A, B](la: List[A], lb: List[B])(implicit ev: A =:= B): List[(A, B)] =
-      for {
-        a <- la
-        b <- lb
-      } yield (a, b)
-
+    def processListSameTypeV3[A, B](la: List[A], lb: List[B])(using ev: A =:= B): List[(A, B)] =
+      processLists(la, lb) // compiler error
 
   }
+
+  val combinedLists = processLists(List(1, 2, 3), List("a", "b", "c")) // "ok"
+  val notOkLists = processLists(List(1, 2, 3), List(4, 5)) // "not ok"
+
+
+  // val combinedNotSameTypeLists = processListSameTypeV3(List(1, 2, 3), List("a", "b", "c")) // "not ok"
+  val okLists = processListSameTypeV3(List(1, 2, 3), List(4, 5)) // "ok"
+
+  object DifferentTypes {
+
+    import scala.util.NotGiven
+
+    // compiler can't find  a given T  => compiler will generate a NotGiven[T]
+    def processListsDifferentType[A, B](la: List[A], lb: List[B])(using NotGiven[A =:= B]): List[(A, B)] =
+      processLists(la, lb)
+
+    val combinedLists_ok = processListsDifferentType(List(1, 2, 3), List("a", "b", "c")) // "ok"
+    // val notOkLists_shouldNotCOmpile = processListsDifferentType(List(1, 2, 3), List(4, 5)) // "not ok"
+
+  }
+
 
 
 }
